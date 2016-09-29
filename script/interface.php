889dad5ca6238d9d38f074209e753a8ffc99fabf
florian HENRY <florian.henry@atm-consulting.fr>
2016-09-29 10:56:24 +0200

    Add intervention

--- script/interface.php
+++ script/interface.php
@@ -1,51 +1,51 @@
 <?php
 
 	require '../config.php';
-	
+
 	$get = GETPOST('get');
-	
+
 	switch ($get) {
 		case 'search':
-			
+
 			__out(_search(GETPOST('key')),'json' );
-			
+
 			break;
-		
+
 		default:
-			
+
 			break;
 	}
 
 function _search($keyword) {
 	global $db, $conf, $langs;
-	
+
 	$Tab = array();
-	
-	$TType=array('invoice','commande','propal','projet','task','company','contact','event', 'product', 'facture_fournisseur', 'commande_fournisseur');
-	
+
+	$TType=array('invoice','commande','propal','projet','task','company','contact','event', 'product', 'facture_fournisseur', 'commande_fournisseur','fichinter');
+
 	if(!empty($conf->of->enabled)) {
 		$TType[] = 'ordre_fabrication';
 	}
-	
+
 	//$TType=array('facture_fournisseur', 'commande_fournisseur');
 	foreach($TType as $type) {
 		$Tab[$type] = _search_type($type, $keyword);
 	}
-	
+
 	return $Tab;
-	
+
 }
-	
+
 function _search_type($type, $keyword) {
 	global $db, $conf, $langs;
-	
+
 	$table = MAIN_DB_PREFIX.$type;
 	$objname = ucfirst($type);
 	$id_field = 'rowid';
 	$ref_field = 'ref';
 	$ref_field2 = '';
 	$join_to_soc = false;
-	
+
 	if($type == 'company') {
 		$table = MAIN_DB_PREFIX.'societe';
 		$objname = 'Societe';
@@ -68,7 +68,7 @@ function _search_type($type, $keyword) {
 		$objname = 'ActionComm';
 		$id_field = 'id';
 		$ref_field = 'id';
-		$ref_field2 = 'label'; 
+		$ref_field2 = 'label';
 		$join_to_soc = true;
 	}
 	elseif($type == 'order' || $type == 'commande') {
@@ -95,7 +95,7 @@ function _search_type($type, $keyword) {
     elseif($type == 'product') {
         $table = MAIN_DB_PREFIX.'product';
         $ref_field = 'ref';
-        
+
     }
     elseif ($type=='facture_fournisseur') {
         $table=MAIN_DB_PREFIX.'facture_fourn';
@@ -110,19 +110,25 @@ function _search_type($type, $keyword) {
         $ref_field='ref';
 		$join_to_soc = true;
     }
+    elseif ($type=='fichinter'){
+    	$table=MAIN_DB_PREFIX.'fichinter';
+    	$objname='Fichinter';
+    	$ref_field='ref';
+    	$join_to_soc = true;
+    }
 	else if(!empty($conf->of->enabled) && $type == 'ordre_fabrication') {
 		$table=MAIN_DB_PREFIX.'assetOf';
         $objname='TAssetOf';
         $ref_field='numero';
-		
+
 	}
-	
-	
+
+
 	$Tab = array();
-	
-    
+
+
 	$sql = "SELECT t.".$id_field." as rowid, CONCAT(t.".$ref_field." ".( empty($ref_field2) ? '' : ",' ',t.".$ref_field2 )." ) as ref ";
-	
+
 	if($join_to_soc) {
 		if($type == 'task') {
 			$sql.=",CONCAT(p.title,', ',s.nom) as client";
@@ -132,21 +138,21 @@ function _search_type($type, $keyword) {
 		}
 		else {
 			$sql.=",s.nom as client";
-		}	
-	} 
-	 
+		}
+	}
+
 	$sql.=" FROM ".$table." as t ";
-	
+
 	if($join_to_soc) {
 		if($type == 'task') {
 			$sql.=" LEFT JOIN ".MAIN_DB_PREFIX."projet p ON (p.rowid = t.fk_projet) ";
 			$sql.=" LEFT JOIN ".MAIN_DB_PREFIX."societe s ON (s.rowid = p.fk_soc) ";
 		}
 		else {
-			$sql.=" LEFT JOIN ".MAIN_DB_PREFIX."societe s ON (s.rowid = t.fk_soc) ";	
+			$sql.=" LEFT JOIN ".MAIN_DB_PREFIX."societe s ON (s.rowid = t.fk_soc) ";
 		}
 	}
-	
+
 	if ($db->type == 'pgsql' && ($ref_field=='id' || $ref_field=='rowid')) {
 		$sql.=" WHERE CAST(t.".$ref_field." AS TEXT) LIKE '".$keyword."%' ";
 	} else {
@@ -158,34 +164,34 @@ function _search_type($type, $keyword) {
 	} elseif (!empty($ref_field2)) {
 		$sql.=" OR t.".$ref_field2." LIKE '".$keyword."%' ";
 	}
-	
+
 	$sql.=" LIMIT 20 ";
 	//var_dump($sql);
     //$sql="SELECT ff.ref FROM  ".MAIN_DB_PREFIX."facture_fourn ff WHERE ";
 	$res = $db->query($sql);
-	
+
 	if($res === false) {
 		pre($db,true);
 	}
-	
+
 	$nb_results = $db->num_rows($res);
-	
-	
+
+
 	if($nb_results == 0) {
 		return array();
 	}
 	else{
 		while($obj = $db->fetch_object($res)) {
-			
+
 			$r = $obj->ref;
 			if(!empty($obj->client))$r.=', '.$obj->client;
-			
+
 			$Tab[$obj->rowid] = $r;
-			
+
 		}
-		return $Tab;	
+		return $Tab;
 	}
-	
-	
-	
+
+
+
 }
